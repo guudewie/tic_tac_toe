d40e99c288dbf34edf67952b95076d7045ce0a54
guudewie <philippthiessen12@gmail.com>
2023-07-09 18:50:58 +0200

    add gameboard module

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("lol")
+/*
+
+MODULE 
+ - gamboard
+ - displayController
+
+FACTORIES
+ - players
+
+*/
+
+const gameboard = (() => {
+
+    let fields = ["X", "X", "X", "O", "O", "O", "X", "X", "X"];
+    let _domFields = document.querySelectorAll(".field");
+
+    function renderFields() {
+        for (f in fields) {
+            _domFields[f].textContent = fields[f]
+        }
+    };
+
+    return {
+        fields,
+        _domFields,
+        renderFields
+    };
+})();
+
+
+console.log(gameboard.renderFields())
+
+
+
